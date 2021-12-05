969068ec6f1394dc0308bf115fcfaa5a0d36882e
Hernán Asorey <asoreyh@gmail.com>
2021-12-06 00:03:48 +0100

    Perl::Switch module is not required anymore

--- sims/generate_spectra.pl
+++ sims/generate_spectra.pl
@@ -54,7 +54,7 @@
 $VERSION="v1r2";
 
 # defaults
-use Switch;
+# use Switch;
 $pi=3.1415927;
 
 $site="unk";
@@ -300,258 +300,220 @@ $atmcrd = "ATMOSPHERE";
 # New set of LAGO sites in preparation for EOSC challenge. 
 # For pre-challenge we will simulate some of this new sites. 
 # HA - Apr 08, 2021
+# switch case replaced by if-elsif-else
 
-switch ($site) {
-  case "sawb" {
-      $modatm="E5";
-      $altitude=20000;
-      $bx=19.366;
-      $bz=-30.222;
-  }
-  case "mapi" {
-      $modatm="E5";
-      $altitude=1000;
-      $bx=19.309;
-      $bz=-28.693;
-  }
-  case "brc" {
-      $modatm="E3";
-      $altitude=86500;
-      $bx=18.952;
-      $bz=-17.05;
-  }
-  case "bue" {
-      $modatm="E2";
-      $altitude=1000;
-      $bx=17.09;
-      $bz=-14.673;
-  }
-  case "kna" {
-      $modatm="E2";
-      $altitude=34700;
-      $bx=19.56;
-      $bz=-13.333;
-  }
-  case "lsc" {
-      $modatm="E2";
-      $altitude=2800;
-      $bx=19.975;
-      $bz=-11.826;
-  }
-  case "tuc" {
-      $modatm="E2";
-      $altitude=43000;
-      $bx=19.487;
-      $bz=-10.647;
-  }
-  case "asu" {
-      $modatm="E1";
-      $altitude=13600;
-      $bx=18.233;
-      $bz=-11.73;
-  }
-  case "sao" {
-      $modatm="E1";
-      $altitude=76000;
-      $bx=16.484;
-      $bz=-14.48;
-  }
-  case "vcp" {
-      $modatm="E1";
-      $altitude=64000;
-      $bx=16.751;
-      $bz=-14.104;
-  }
-  case "lpb" {
-      $modatm="E2";
-      $altitude=363000;
-      $bx=22.362;
-      $bz=-4.708;
-  }
-  case "cha" {
-      $modatm="E2";
-      $altitude=523000;
-      $bx=22.38;
-      $bz=-4.62;
-  }
-  case "cuz" {
-      $modatm="E1";
-      $altitude=340000;
-      $bx=23.676;
-      $bz=-2.02;
-  }
-  case "lim" {
-      $modatm="E1";
-      $altitude=16800;
-      $bx=24.723;
-      $bz=-0.476;
-  }
-  case "cpv" {
-      $modatm="E1";
-      $altitude=55000;
-      $bx=21.174;
-      $bz=-12.612;
-  }
-  case "serb" {
-      $modatm="E1";
-      $altitude=275000;
-      $bx=26.577;
-      $bz=8.499;
-  }
-  case "QUIE" {
-      $modatm="E1";
-      $altitude=285000;
-      $bx=26.717;
-      $bz=9.971;
-  }
-  case "bga" {
-      $modatm="E1";
-      $altitude=95000;
-      $bx=26.793;
-      $bz=16.055;
-  }
-  case "pam" {
-      $modatm="E1";
-      $altitude=234200;
-      $bx=26.743;
-      $bz=15.955;
-  }
-  case "gua" {
-      $modatm="E1";
-      $altitude=149000;
-      $bx=27.532;
-      $bz=24.932;
-  }
-  case "tac" {
-      $modatm="E1";
-      $altitude=406000;
-      $bx=27.53;
-      $bz=25.353;
-  }
-  case "chia" {
-      $modatm="E2";
-      $altitude=52200;
-      $bx=27.405;
-      $bz=27.024;
-  }
-  case "chi" {
-      $modatm="E1";
-      $altitude=500000;
-      $bx=26.56;
-      $bz=8.758;
-  }
-  case "sng" {
-      $modatm="E2";
-      $altitude=455000;
-      $bx=27.358;
-      $bz=28.038;
-  }
-  case "and" {
-      $modatm="E2";
-      $altitude=420000;
-      $bx=19.658;
-      $bz=-11.951;
-  }
-  case "mge" {
-      $modatm="E2";
-      $altitude=145000;
-      $bx=18.987;
-      $bz=-14.326;
-  }
-  case "ber" {
-      $modatm="E1";
-      $altitude=345000;
-      $bx=26.751;
-      $bz=16.029;
-  }
-  case "sac" {
-      $modatm="E2";
-      $altitude=482000;
-      $bx=20.06;
-      $bz=-9.616;
-  }
-  case "cop" {
-      $modatm="E2";
-      $altitude=200000;
-      $bx=19.04;
-      $bz=-15.493;
-  }
-  case "sgd" {
-      $modatm="E2";
-      $altitude=25000;
-      $bx=18.108;
-      $bz=-16.891;
-  }
-  case "casp" {
-      $modatm="E2";
-      $altitude=260000;
-      $bx=19.473;
-      $bz=-12.436;
-  }
-  case "ppet" {
-      $modatm="E2";
-      $altitude=350000;
-      $bx=19.139;
-      $bz=-14.27;
-  }
-  case "mad" {
-      $modatm="E2";
-      $altitude=70000;
-      $bx=25.647;
-      $bz=36.933;
-  }
-  case "truj" {
-      $modatm="E2";
-      $altitude=56000;
-      $bx=26.223;
-      $bz=35.844;
-  }
-  case "pozn" {
-      $modatm="E2";
-      $altitude=10000;
-      $bx=18.598;
-      $bz=46.45;
-  }
-  case "juli" {
-      $modatm="E2";
-      $altitude=10000;
-      $bx=19.676;
-      $bz=44.928;
-  }
-  case "sudb" {
-      $modatm="E2";
-      $altitude=210000;
-      $bx=17.037;
-      $bz=51.991;
-  }
-  case "viri" {
-      $modatm="E2";
-      $altitude=285000;
-      $bx=19.061;
-      $bz=-16.289;
-  }
-  case "ima" {
-      $modatm="E1";
-      $altitude=460000;
-      $bx=22.969;
-      $bz=-3.79;
-  }
-  case "ata" {
-      $modatm="E1";
-      $altitude=510500;
-      $bx=20.638;
-      $bz=-8.598;
-  }
-  case "gen" {
-      $gensite=1;
-  }
-  case "air" {
-      $gensite=1;
-  }
-  case "unk" {
-      $gensite=1;
-  }
-}#switch
+my $modatm="";
+my $altitude=0.;
+my $bx=0.;
+my $bz=0.;
+
+if ($site eq "sawb") {
+    $modatm="E5";
+    $altitude=20000;
+    $bx=19.366;
+    $bz=-30.222;
+} elsif ($site eq "mapi") {
+    $modatm="E5";
+    $altitude=1000;
+    $bx=19.309;
+    $bz=-28.693;
+} elsif ($site eq "brc") {
+    $modatm="E3";
+    $altitude=86500;
+    $bx=18.952;
+    $bz=-17.05;
+} elsif ($site eq "bue") {
+    $modatm="E2";
+    $altitude=1000;
+    $bx=17.09;
+    $bz=-14.673;
+} elsif ($site eq "kna") {
+    $modatm="E2";
+    $altitude=34700;
+    $bx=19.56;
+    $bz=-13.333;
+} elsif ($site eq "lsc") {
+    $modatm="E2";
+    $altitude=2800;
+    $bx=19.975;
+    $bz=-11.826;
+} elsif ($site eq "tuc") {
+    $modatm="E2";
+    $altitude=43000;
+    $bx=19.487;
+    $bz=-10.647;
+} elsif ($site eq "asu") {
+    $modatm="E1";
+    $altitude=13600;
+    $bx=18.233;
+    $bz=-11.73;
+} elsif ($site eq "sao") {
+    $modatm="E1";
+    $altitude=76000;
+    $bx=16.484;
+    $bz=-14.48;
+} elsif ($site eq "vcp") {
+    $modatm="E1";
+    $altitude=64000;
+    $bx=16.751;
+    $bz=-14.104;
+} elsif ($site eq "lpb") {
+    $modatm="E2";
+    $altitude=363000;
+    $bx=22.362;
+    $bz=-4.708;
+} elsif ($site eq "cha") {
+    $modatm="E2";
+    $altitude=523000;
+    $bx=22.38;
+    $bz=-4.62;
+} elsif ($site eq "cuz") {
+    $modatm="E1";
+    $altitude=340000;
+    $bx=23.676;
+    $bz=-2.02;
+} elsif ($site eq "lim") {
+    $modatm="E1";
+    $altitude=16800;
+    $bx=24.723;
+    $bz=-0.476;
+} elsif ($site eq "cpv") {
+    $modatm="E1";
+    $altitude=55000;
+    $bx=21.174;
+    $bz=-12.612;
+} elsif ($site eq "serb") {
+    $modatm="E1";
+    $altitude=275000;
+    $bx=26.577;
+    $bz=8.499;
+} elsif ($site eq "quie") {
+    $modatm="E1";
+    $altitude=285000;
+    $bx=26.717;
+    $bz=9.971;
+} elsif ($site eq "bga") {
+    $modatm="E1";
+    $altitude=95000;
+    $bx=26.793;
+    $bz=16.055;
+} elsif ($site eq "pam") {
+    $modatm="E1";
+    $altitude=234200;
+    $bx=26.743;
+    $bz=15.955;
+} elsif ($site eq "gua") {
+    $modatm="E1";
+    $altitude=149000;
+    $bx=27.532;
+    $bz=24.932;
+} elsif ($site eq "tac") {
+    $modatm="E1";
+    $altitude=406000;
+    $bx=27.53;
+    $bz=25.353;
+} elsif ($site eq "chia") {
+    $modatm="E2";
+    $altitude=52200;
+    $bx=27.405;
+    $bz=27.024;
+} elsif ($site eq "chi") {
+    $modatm="E1";
+    $altitude=500000;
+    $bx=26.56;
+    $bz=8.758;
+} elsif ($site eq "sng") {
+    $modatm="E2";
+    $altitude=455000;
+    $bx=27.358;
+    $bz=28.038;
+} elsif ($site eq "and") {
+    $modatm="E2";
+    $altitude=420000;
+    $bx=19.658;
+    $bz=-11.951;
+} elsif ($site eq "mge") {
+    $modatm="E2";
+    $altitude=145000;
+    $bx=18.987;
+    $bz=-14.326;
+} elsif ($site eq "ber") {
+    $modatm="E1";
+    $altitude=345000;
+    $bx=26.751;
+    $bz=16.029;
+} elsif ($site eq "sac") {
+    $modatm="E2";
+    $altitude=482000;
+    $bx=20.06;
+    $bz=-9.616;
+} elsif ($site eq "cop") {
+    $modatm="E2";
+    $altitude=200000;
+    $bx=19.04;
+    $bz=-15.493;
+} elsif ($site eq "sgd") {
+    $modatm="E2";
+    $altitude=25000;
+    $bx=18.108;
+    $bz=-16.891;
+} elsif ($site eq "casp") {
+    $modatm="E2";
+    $altitude=260000;
+    $bx=19.473;
+    $bz=-12.436;
+} elsif ($site eq "ppet") {
+    $modatm="E2";
+    $altitude=350000;
+    $bx=19.139;
+    $bz=-14.27;
+} elsif ($site eq "mad") {
+    $modatm="E2";
+    $altitude=70000;
+    $bx=25.647;
+    $bz=36.933;
+} elsif ($site eq "truj") {
+    $modatm="E2";
+    $altitude=56000;
+    $bx=26.223;
+    $bz=35.844;
+} elsif ($site eq "pozn") {
+    $modatm="E2";
+    $altitude=10000;
+    $bx=18.598;
+    $bz=46.45;
+} elsif ($site eq "juli") {
+    $modatm="E2";
+    $altitude=10000;
+    $bx=19.676;
+    $bz=44.928;
+} elsif ($site eq "sudb") {
+    $modatm="E2";
+    $altitude=210000;
+    $bx=17.037;
+    $bz=51.991;
+} elsif ($site eq "viri") {
+    $modatm="E2";
+    $altitude=285000;
+    $bx=19.061;
+    $bz=-16.289;
+} elsif ($site eq "ima") {
+    $modatm="E1";
+    $altitude=460000;
+    $bx=22.969;
+    $bz=-3.79;
+} elsif ($site eq "ata") {
+    $modatm="E1";
+    $altitude=510500;
+    $bx=20.638;
+    $bz=-8.598;
+} elsif ($site eq "gen") {
+    $gensite=1;
+} elsif ($site eq "air") {
+    $gensite=1;
+} elsif ($site eq "unk") {
+    $gensite=1;
+}
 
 unless ($ifixmodatm) {
     $modatm = get("Atmospheric model selection (ATMOD). Start number with 'E' to use external atmospheres module (ATMOSPHERE), examples: E30=wi,E31=sp,E32=su,E33=au)", $modatm, "$atmcrd", $modatm);
